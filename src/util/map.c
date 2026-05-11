4be8bc128b32219f7b930a7d8daf907718bc1c5a
Hudson Schumaker <hudson.schumaker@me.com>
2026-05-11 18:03:33 +0200

    Dodoi-Lab: add map stubs.

---
+++ src/util/map.c
@@ -0,0 +1,8 @@
+/**
+* @file map.c
+* @author Hudson Schumaker
+*
+* Dodoi-Engine is a game engine developed by Dodoi-Lab.
+* @copyright Copyright (c) 2024, Dodoi-Lab
+*/
+#include "map.h"
---
+++ src/util/map.h
@@ -0,0 +1,9 @@
+/**
+* @file map.h
+* @author Hudson Schumaker
+*
+* Dodoi-Engine is a game engine developed by Dodoi-Lab.
+* @copyright Copyright (c) 2024, Dodoi-Lab
+*/
+#pragma once
+#include "../pch.h"
