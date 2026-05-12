11122bb1965b5700942cc77cfb14bf926e9af705
Hudson Schumaker <hudson.schumaker@me.com>
2026-05-12 17:38:54 +0200

    Dodoi-Lab: add map.

--- src/util/map.c
+++ src/util/map.c
@@ -6,3 +6,29 @@
 * @copyright Copyright (c) 2024, Dodoi-Lab
 */
 #include "map.h"
+#include "../gfx/gfx.h"
+
+void map_load(map_t* map, const char* data[MAP_HEIGHT][MAP_WIDTH]) {
+    for (short y = 0; y < MAP_HEIGHT; ++y) {
+        for (short x = 0; x < MAP_WIDTH; ++x) {
+            char type_char = data[y][x][0];
+            short tex_id = atoi(&data[y][x][1]);
+
+            if (type_char == 'p') {
+                map->tiles[y][x].type = TILE_PATH;
+            } else if (type_char == 'g') {
+                map->tiles[y][x].type = TILE_GROUND;
+            }
+            map->tiles[y][x].texture_id = tex_id;
+        }
+    }
+}
+
+void map_render(const map_t* map) {
+    for (short y = 0; y < MAP_HEIGHT; ++y) {
+        for (short x = 0; x < MAP_WIDTH; ++x) {
+            short tex_id = map->tiles[y][x].texture_id;
+            gfx_render_texture(map->textures[tex_id], x * MAP_TILE_SIZE, y * MAP_TILE_SIZE);
+        }
+    }
+}
--- src/util/map.h
+++ src/util/map.h
@@ -7,3 +7,21 @@
 */
 #pragma once
 #include "../pch.h"
+
+typedef enum {
+    TILE_PATH,
+    TILE_GROUND
+} map_tile_type_t;
+
+typedef struct {
+    map_tile_type_t type;
+    short texture_id;
+} map_tile_t;
+
+typedef struct {
+    SDL_Texture* textures[8];
+    map_tile_t tiles[MAP_HEIGHT][MAP_WIDTH];
+} map_t;
+
+void map_load(map_t* map, const char* data[MAP_HEIGHT][MAP_WIDTH]);
+void map_render(const map_t* map);
