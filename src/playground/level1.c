5823ba1fb10bbb3a0117ff255932fbd7fd96d87a
Hudson Schumaker <hudson.schumaker@me.com>
2026-05-13 19:06:49 +0200

    Dodoi-Lab: add level1.

--- src/playground/level1.c
+++ src/playground/level1.c
@@ -6,17 +6,24 @@
 * Dodoi-Engine is a game engine developed by Dodoi-Lab.
 * @copyright Copyright (c) 2024, Dodoi-Lab
 */
-
+#include "enemy.h"
+#include "tower.h"
 #include "level1.h"
 #include "../gfx/gfx.h"
 #include "../util/map.h"
 #include "../core/engine.h"
 #include "grass_png.h"
 #include "path_png.h"
+#include "orc_png.h"
+#include "t1_png.h"
 
 static bool running = false;
 static scene_t* level1 = NULL;
 static map_t map;
+static enemy_t enemie;
+static tower_t towers;
+static SDL_Texture* enemy_texture = NULL;
+static SDL_Texture* tower_texture = NULL;
 
 static const char* level1_data[MAP_HEIGHT][MAP_WIDTH] = {
     { "g0","g0","g0","g0","g0","g0","g0","g0","g0","g0","g0","g0","g0","g0","g0","g0","g0","g0","g0","g0" },
@@ -54,19 +61,32 @@ void level1_load(void) {
     map_load(&map, level1_data);
     map.textures[0] = gfx_load_texture(grass_png, grass_png_size);
     map.textures[1] = gfx_load_texture(path_png, path_png_size);
+    enemy_texture = gfx_load_texture(orc_png, orc_png_size);
+    tower_texture = gfx_load_texture(t1_png, t1_png_size);
+
+    enemie = enemy(0, 7 * MAP_TILE_SIZE, MAP_TILE_SIZE, MAP_TILE_SIZE, 100, 25.0f);
+    enemie.texture = enemy_texture;
+
+    towers = tower(5 * MAP_TILE_SIZE, 6 * MAP_TILE_SIZE, MAP_TILE_SIZE, MAP_TILE_SIZE, 100, 10, 1);
+    towers.texture = tower_texture;
+
     running = true;
 }
 
 void level1_input(void) {}
 
 void level1_update(void) {
-    engine_calculate_delta_time();
+    float delta_time = engine_calculate_delta_time();
+    enemy_update(&enemie, delta_time);
+    tower_update(&towers, &enemie, delta_time);
 }
 
 void level1_render(void) {
     scene_begin_render();
     {
         map_render(&map);
+        enemy_render(&enemie);
+        tower_render(&towers);
     }
     scene_end_render();
 }
@@ -88,6 +108,14 @@ void level1_unload(void) {
     }
     free(level1);
     level1 = NULL;
+
+    if (enemy_texture) {
+        SDL_DestroyTexture(enemy_texture);
+    }
+
+    if (tower_texture) {
+        SDL_DestroyTexture(tower_texture);
+    }
 }
 
 scene_t* level1_get_scene(void) {
