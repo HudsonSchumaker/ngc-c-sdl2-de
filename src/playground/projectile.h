4fd27eec464a1709b9ebaa825c3e92ed5a236f0d
HudsonSchumaker <hudson.schumaker@me.com>
2026-05-20 18:52:03 +0200

    Dodoi-Lab: add projectile header.

---
+++ src/playground/projectile.h
@@ -0,0 +1,20 @@
+/**
+* @file projectile.h
+* @author Hudson Schumaker
+*
+* Dodoi-Engine is a game engine developed by Dodoi-Lab.
+* @copyright Copyright (c) 2024, Dodoi-Lab
+*/
+#pragma once
+#include "../pch.h"
+
+typedef struct {
+    f32 speed[MAX_ENTITIES];
+    f32 damage[MAX_ENTITIES];
+    f32 ox[MAX_ENTITIES];
+    f32 oy[MAX_ENTITIES];
+    f32 dx[MAX_ENTITIES];
+    f32 dy[MAX_ENTITIES];
+    f32 tx[MAX_ENTITIES];
+    f32 ty[MAX_ENTITIES];
+} projectile_pool_t;
